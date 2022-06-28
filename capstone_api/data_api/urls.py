6b3f847b9633466885ff7934d152bb3ce3854d65
Lanny MacMillan <LannyMacMillan@gmail.com>
2022-06-28 00:21:37 -0400

    backend model change

--- capstone_api/data_api/urls.py
+++ capstone_api/data_api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/events', views.VacationEventList.as_view(), name='VacationEvent_list'), # api/VacationEvent will be routed to the VacationEventList view for handling
-    path('api/events/<int:pk>', views.VacationEventDetail.as_view(), name='VacationEvent_detail'), # api/VacationEvent will be routed to the EventDetail view for handling
+    path('api/vacationevents', views.VacationEventList.as_view(), name='VacationEvent_list'), # api/VacationEvent will be routed to the VacationEventList view for handling
+    path('api/vacationevents/<int:pk>', views.VacationEventDetail.as_view(), name='VacationEvent_detail'), # api/VacationEvent will be routed to the EventDetail view for handling
 ]
